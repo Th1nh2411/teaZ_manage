888c4f829d769951ed652c8e82c40ce73ed0c67d
Thinh Nguyen <rinktvn2525@gmail.com>
2023-08-07 10:57:54 +0700

    update done quantity ingredient change

--- src/Pages/AdminIngredientPage/QuantityChange.js
+++ src/Pages/AdminIngredientPage/QuantityChange.js
@@ -10,17 +10,18 @@ import { FaFileInvoiceDollar } from 'react-icons/fa';
 
 import Tippy from '@tippyjs/react';
 import dayjs from 'dayjs';
-import { DatePicker, Select, Table } from 'antd';
+import { DatePicker, Divider, Select, Table } from 'antd';
 import { Col, Row } from 'react-bootstrap';
+import ExportFile from '../../components/ExportFile/ExportFile';
 const cx = classNames.bind(styles);
 const importColumns = [
     {
         title: 'id',
-        dataIndex: 'id',
-        key: 'idImport',
+        dataIndex: 'idIngredient',
+        key: 'idIngredient',
     },
     {
-        title: 'Tên nguyên liệu',
+        title: 'Tên',
         dataIndex: 'name',
         key: 'name',
     },
@@ -30,12 +31,7 @@ const importColumns = [
         key: 'unitName',
     },
     {
-        title: 'Trạng thái',
-        dataIndex: 'isActive',
-        key: 'isActive',
-    },
-    {
-        title: 'Số lượng nhập',
+        title: 'SL',
         dataIndex: 'quantity',
         key: 'quantity',
     },
@@ -50,31 +46,88 @@ const importColumns = [
         key: 'date',
     },
 ];
+const exportColumns = [
+    {
+        title: 'id',
+        dataIndex: 'idIngredient',
+        key: 'idIngredient',
+    },
+    {
+        title: 'Tên',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'ĐVT',
+        dataIndex: 'unitName',
+        key: 'unitName',
+    },
+    {
+        title: 'SL',
+        dataIndex: 'quantity',
+        key: 'quantity',
+    },
+    {
+        title: 'Ngày xuất',
+        dataIndex: 'date',
+        key: 'date',
+    },
+    {
+        title: 'Ghi chú',
+        dataIndex: 'info',
+        key: 'info',
+    },
+];
 function QuantityChange() {
-    const [imports, setImports] = useState();
-    const [exportsFromBH, setExportsFromBH] = useState();
-    const [exports, setExports] = useState();
+    const [imports, setImports] = useState([]);
+    const [exportsFromBH, setExportsFromBH] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [exports, setExports] = useState([]);
     const [type, setType] = useState('day');
     const [date, setDate] = useState(dayjs().format('YYYY-MM-DD'));
     const localStorageManager = LocalStorageManager.getInstance();
 
     const getIngredientReport = async () => {
         const token = localStorageManager.getItem('token');
+        setLoading(true);
         if (token) {
             const results = await reportService.getIngredientReportByDate(date, token, type);
-            if (results) {
-                setImports(results.imports);
-                setExportsFromBH(results.exportsBH);
-                setExports(results.exportsWithoutBH);
+            if (results && results.isSuccess) {
+                setImports(
+                    results.imports.map((item) => {
+                        return {
+                            ...item,
+                            total: priceFormat(item.total) + 'đ',
+                            date: item.date,
+                        };
+                    }),
+                );
+                setExportsFromBH(
+                    results.exportsBH.map((item) => {
+                        return {
+                            ...item,
+                            date: item.date,
+                        };
+                    }),
+                );
+                setExports(
+                    results.exportsWithoutBH.map((item) => {
+                        return {
+                            ...item,
+                            date: item.date,
+                        };
+                    }),
+                );
             }
         }
+        setLoading(false);
     };
     useEffect(() => {
         getIngredientReport();
     }, [date, type]);
     console.log(imports);
     return (
-        <div className={cx('content-wrapper')}>
+        <div className={cx('content-wrapper', 'mt-4')}>
             <div className={cx('content-header')}>
                 <div className={cx('content-title')}>
                     <FaFileInvoiceDollar className={cx('icon', 'highlight')} />
@@ -117,7 +170,99 @@ function QuantityChange() {
             <div className={cx('content-body')}>
                 <Row>
                     <Col>
-                        <Table columns={importColumns} dataSource={imports} />
+                        <Divider>
+                            <div className={cx('d-flex', 'align-items-center')}>
+                                Nhập hàng{' '}
+                                <ExportFile
+                                    csvData={imports.map((item) => {
+                                        return {
+                                            id: item.idIngredient,
+                                            'Tên sản phẩm': item.name,
+                                            ĐVT: item.unitName,
+                                            'Số lượng': item.quantity,
+                                            'Tổng tiền': priceFormat(item.total) + 'đ',
+                                            'Ngày nhập': item.date,
+                                        };
+                                    })}
+                                    fileName="importIngredient"
+                                />
+                            </div>
+                        </Divider>
+                        <Table
+                            size="small"
+                            pagination={{
+                                defaultPageSize: 5,
+                                showSizeChanger: true,
+                                pageSizeOptions: ['10', '20', '30'],
+                            }}
+                            loading={loading}
+                            bordered
+                            columns={importColumns}
+                            dataSource={imports}
+                        />
+                    </Col>
+                    <Col>
+                        <Divider>
+                            <div className={cx('d-flex', 'align-items-center')}>
+                                Xuất hàng{' '}
+                                <ExportFile
+                                    csvData={exports.map((item) => {
+                                        return {
+                                            id: item.idIngredient,
+                                            'Tên sản phẩm': item.name,
+                                            ĐVT: item.unitName,
+                                            'Số lượng': item.quantity,
+                                            'Ngày nhập': item.date,
+                                            'Ghi chú': item.info,
+                                        };
+                                    })}
+                                    fileName="exportIngredient"
+                                />
+                            </div>
+                        </Divider>
+                        <Table
+                            size="small"
+                            pagination={{
+                                defaultPageSize: 5,
+                                showSizeChanger: true,
+                                pageSizeOptions: ['10', '20', '30'],
+                            }}
+                            loading={loading}
+                            bordered
+                            columns={exportColumns}
+                            dataSource={exports}
+                        />
+                    </Col>
+                    <Col>
+                        <Divider>
+                            <div className={cx('d-flex', 'align-items-center')}>
+                                Bán hàng{' '}
+                                <ExportFile
+                                    csvData={exportsFromBH.map((item) => {
+                                        return {
+                                            id: item.idIngredient,
+                                            'Tên sản phẩm': item.name,
+                                            ĐVT: item.unitName,
+                                            'Số lượng': item.quantity,
+                                            'Ngày nhập': item.date,
+                                            'Ghi chú': item.info,
+                                        };
+                                    })}
+                                    fileName="SellIngredient"
+                                />
+                            </div>
+                        </Divider>
+                        <Table
+                            size="small"
+                            pagination={{
+                                showSizeChanger: false,
+                                showLessItems: true,
+                            }}
+                            loading={loading}
+                            bordered
+                            columns={exportColumns}
+                            dataSource={exportsFromBH}
+                        />
                     </Col>
                 </Row>
             </div>
