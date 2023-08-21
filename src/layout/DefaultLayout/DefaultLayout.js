0b7e141b07b86c7a61e08278efd98ce211330ee8
Thinh Nguyen <rinktvn2525@gmail.com>
2023-08-21 11:36:41 +0700

    update sidebar mobile

--- src/layout/DefaultLayout/DefaultLayout.js
+++ src/layout/DefaultLayout/DefaultLayout.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import styles from './DefaultLayout.module.scss';
-import { useContext, useEffect, useMemo, useState } from 'react';
+import { useContext, useEffect, useMemo, useRef, useState } from 'react';
 import LocalStorageManager from '../../utils/LocalStorageManager';
 import { StoreContext, actions } from '../../store';
 import Toast from '../../components/Toast/Toast';
@@ -14,6 +14,7 @@ function DefaultLayout({ children }) {
     const localStorageManager = LocalStorageManager.getInstance();
     const [state, dispatch] = useContext(StoreContext);
     const [sideBarShrink, setSideBarShrink] = useState(false);
+    const [mobileMode, setMobileMode] = useState(false);
     const currentPath = useLocation().pathname;
     const root_theme = document.querySelector(':root');
     const setStorageData = () => {
@@ -25,25 +26,45 @@ function DefaultLayout({ children }) {
     };
     useEffect(() => {
         if (sideBarShrink) {
-            root_theme.style.setProperty('--sidebar-width', '64px');
+            if (!mobileMode) {
+                root_theme.style.setProperty('--sidebar-width', '64px');
+            } else {
+                root_theme.style.setProperty('--sidebar-width', '0px');
+            }
         } else {
             root_theme.style.setProperty('--sidebar-width', '250px');
         }
-    }, [sideBarShrink]);
+    }, [sideBarShrink, mobileMode]);
     useEffect(() => {
         setStorageData();
         function updateSize() {
             if (window.innerWidth < 892) {
-                setSideBarShrink(true);
+                // setSideBarShrink(true);
+                setMobileMode(true);
+            } else {
+                setMobileMode(false);
             }
         }
         window.addEventListener('resize', updateSize);
         updateSize();
         return () => window.removeEventListener('resize', updateSize);
     }, []);
+    const overlayRef = useRef();
+    const handleDocumentClick = (event) => {
+        if (overlayRef.current && overlayRef.current.contains(event.target)) {
+            setSideBarShrink(true);
+        }
+    };
+    useEffect(() => {
+        document.addEventListener('click', handleDocumentClick);
+        return () => {
+            document.removeEventListener('click', handleDocumentClick);
+        };
+    }, []);
     return (
         <>
             <div className={cx('wrapper')}>
+                {mobileMode && !sideBarShrink && <div ref={overlayRef} className={cx('overlay')}></div>}
                 <SideBar sideBarShrink={sideBarShrink} />
                 <div className={cx('container')}>
                     <Header toggleSideBar={handleToggleSideBar} />
